66725c70da79680765e2df3273ced197756a9ba4
VNANDHINIVIJAY <nandhini1013@gmail.com>
2025-08-10 22:48:07 +0530

    Update Patterns.py

--- Patterns.py
+++ Patterns.py
@@ -1,1 +1,55 @@
+//Pattern7
+5
+        *         
+      * * *       
+    * * * * *     
+  * * * * * * *   
+* * * * * * * * * 
+n = int(input())
+for i in range(n):
+    for j in range(0, n-i-1):
+        print(' ', end = ' ')
+    for k in range(0, 2*i+1):
+        print('*', end = ' ')
+    for l in range(0, n-i-1):
+        print(' ', end = ' ')
+    print()
 
+//Pattern8
+5
+        *         
+      * * *       
+    * * * * *     
+  * * * * * * *   
+* * * * * * * * * 
+n = int(input())
+for i in range(n):
+    for j in range(0, i):
+        print(' ', end = ' ')
+    for k in range(0, 2*n-(2*i+1)):
+        print('*', end = ' ')
+    for l in range(0, i):
+        print(' ', end = ' ')
+    print()
+
+//Pattern9
+5
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+n = int(input())
+for i in range(1, 2*n-1+1):
+    stars = i
+    if i > n:
+        stars = 2*n - i
+    for j in range(1, stars+1):
+        print('*', end=' ')
+    print()
+    
+    
